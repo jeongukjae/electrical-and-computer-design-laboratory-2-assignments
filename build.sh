a46fc82a776368408d2211904719c835e9c34d4b
JeongUkJae <JeongUkJae@gmail.com>
2018-09-29 01:09:47 +0900

    build 쉘 스크립트 수정

--- build.sh
+++ build.sh
@@ -1,24 +1,86 @@
 #!/bin/bash
 
+# a script to compile and execute the test bench written with verilog.
+# Usage
+#   # to clear temp
+#   ./build.sh clear
+#
+#   # to build (example: week3/pre/and_gate)
+#   ./build.sh build week3/pre/and_gate
+#
+#   # to build and open gtkwave (example: week3/pre/and_gate)
+#   ./build.sh build week3/pre/and_gate -wave
+
 TEMP_DIR=tmp
 
-# 임시 폴더 비우기
-echo "[exec] rm -rf $TEMP_DIR"
-rm -rf $TEMP_DIR
-echo "[exec] mkdir $TEMP_DIR"
-mkdir $TEMP_DIR
+# main function
+function main() {
+  if [[ $# = 0 ]]; then
+    # no command
+    echo "no command."
+    echo "Available commands: \`build\`, \`clear\`"
+    echo
+    exit
+  fi
 
-# 컴파일
-echo "[exec] iverilog -o $TEMP_DIR/`echo $1 | sed -e 's/.*\///g'`.out `ls -d -- $1/*.v | tr '\n' ' '`"
-iverilog -o $TEMP_DIR/`echo $1 | sed -e 's/.*\///g'`.out `ls -d -- $1/*.v | tr '\n' ' '`
+  if [[ $1 = 'clear' ]]; then
+    # clear command
+    echo "Clear temp dir"
+    clear_temp
+  elif [[ $1 = 'build' ]]; then
+    # build command
+    if [[ $# = 1 ]]; then
+      # error!
+      echo "input path is not specified."
+      echo "Usage: ./build.sh build project_path [-wave]"
+      echo
+      exit
+    fi
 
-# 결과물 실행
-echo "[exec] cd $TEMP_DIR"
-cd $TEMP_DIR
-echo "[exec] vvp $1.out"
-vvp `echo $1 | sed -e 's/.*\///g'`.out
+    echo "Build project $2"
+    clear_temp
+    compile_and_execute $2
+    if [[ $3 = '-wave' ]]; then
+      # when the wave option is set
+      open_gtkwave
+    fi 
+  else
+    # wrong command
+    echo "A command \`$1\` is not supported."
+    echo "Available commands: \`build\`, \`clear\`"
+    echo
+    exit
+  fi
+}
 
-if [[ $2 = '-wave' ]]; then
-  # open gtkwave
-  open -a gtkwave *.vcd
-fi
+# print the command and execute it
+function log() {
+  echo "[exec] $@"
+  $@
+}
+
+# remove temp directory and recreate it
+function clear_temp() {
+  log rm -rf $TEMP_DIR
+  log mkdir $TEMP_DIR
+}
+
+# compile project and execute it to create vcd file
+function compile_and_execute() {
+  OUTPUT_BASE_NAME=`echo $1 | sed -e 's/.*\///g'`
+  INPUT_FILES=`ls -d -- $1/*.v | tr '\n' ' '`
+
+  # compile with iverilog
+  log iverilog -o $TEMP_DIR/$OUTPUT_BASE_NAME.out $INPUT_FILES
+
+  # execute out file to create vcd file
+  log cd $TEMP_DIR
+  log vvp $OUTPUT_BASE_NAME.out
+}
+
+# open gtkwave
+function open_gtkwave() {
+  log open -a gtkwave *.vcd
+}
+
+main $@
